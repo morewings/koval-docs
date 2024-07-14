c96cdd7e632356ad44c37b4fb8f200796eb5e918
Dima Vyshniakov <morewings@gmail.com>
2024-07-14 14:57:32 +0200

    Add input API docs

--- components/Iframe/Iframe.tsx
+++ components/Iframe/Iframe.tsx
@@ -1,12 +1,27 @@
-import {FC} from 'react';
+import {FC, useMemo} from 'react';
+import {useLocalTheme} from 'css-vars-hook';
 import classes from './Iframe.module.css';
 
 export type Props = {
     src: string;
     width: number;
     height: number;
+    caption?: string;
 }
 
-export const Iframe: FC<Props> = ({src, height, width}) => {
-    return <iframe className={classes.iframe} src={src} width={width} height={height} />
+const getPercentage = (width: number, height: number) => (height/width) * 100
+
+export const Iframe: FC<Props> = ({src, height, width, caption}) => {
+    const {LocalRoot} = useLocalTheme();
+    const theme = useMemo(() => {
+        return {size: `${getPercentage(width, height)}%`}
+    }, [width, height]);
+    return (
+        <LocalRoot theme={theme}>
+            <div className={classes.wrapper}>
+                <iframe className={classes.iframe} src={src} width={width} height={height} />
+            </div>
+            {caption && <div className={classes.caption}>{caption}</div>}
+        </LocalRoot>
+    );
 }
