8cfd536338da6d09a3d69b8ba034b8c0fedd6b93
Dima Vyshniakov <morewings@gmail.com>
2024-08-20 00:38:50 +0200

    Add Piwik 3

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires,no-undef
+// eslint-disable-next-line no-undef,@typescript-eslint/no-require-imports
 const withNextra = require('nextra')({
     theme: 'nextra-theme-docs',
     themeConfig: './theme.config.tsx',
+    transpilePackages: ['@piwikpro/next-piwik-pro'],
 });
 
 // eslint-disable-next-line no-undef
